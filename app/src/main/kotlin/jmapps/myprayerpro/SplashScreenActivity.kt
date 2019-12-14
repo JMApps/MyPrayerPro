2aeea255b31839e38cd9f37495bd5d34a5753e4f
dev_muslim <jmappsmuslim@gmail.com>
2019-12-14 10:28:24 +0300

    Added splash screen with icon and theme

---
+++ app/src/main/kotlin/jmapps/myprayerpro/SplashScreenActivity.kt
@@ -0,0 +1,15 @@
+package jmapps.myprayerpro
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class SplashScreenActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+}
