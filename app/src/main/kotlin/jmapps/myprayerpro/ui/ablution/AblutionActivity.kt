5579806d607cf4889669a78ce2924c7d068afedb
dev_muslim <jmappsmuslim@gmail.com>
2019-12-16 15:28:50 +0300

    Added ablution activity

---
+++ app/src/main/kotlin/jmapps/myprayerpro/ui/ablution/AblutionActivity.kt
@@ -0,0 +1,26 @@
+package jmapps.myprayerpro.ui.ablution
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import jmapps.myprayerpro.R
+import kotlinx.android.synthetic.main.activity_ablution.*
+
+class AblutionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ablution)
+        setSupportActionBar(toolbar)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
